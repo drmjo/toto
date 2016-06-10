847bb3d02ca921a1745ab28382a362cae10e7305
Majan Paul <drmjo007@gmail.com>
2016-06-09 21:06:20 -0700

    relationship fix

--- src/TotoBundle/Entity/Team.php
+++ src/TotoBundle/Entity/Team.php
@@ -28,23 +28,9 @@ class Team
     private $updatedAt;
 
     /**
-     * @var \Doctrine\Common\Collections\Collection
+     * @var \TotoBundle\Entity\Group
      */
-    private $homeGames;
-
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     */
-    private $awayGames;
-
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->homeGames = new \Doctrine\Common\Collections\ArrayCollection();
-        $this->awayGames = new \Doctrine\Common\Collections\ArrayCollection();
-    }
+    private $group;
 
     /**
      * Get id
@@ -105,8 +91,6 @@ class Team
     /**
      * Set updatedAt
      *
-     * @param \DateTime $updatedAt
-     *
      * @return Team
      */
     public function setUpdatedAt()
@@ -125,79 +109,6 @@ class Team
     {
         return $this->updatedAt;
     }
-
-    /**
-     * Add homeGame
-     *
-     * @param \TotoBundle\Entity\Game $homeGame
-     *
-     * @return Team
-     */
-    public function addHomeGame(\TotoBundle\Entity\Game $homeGame)
-    {
-        $this->homeGames[] = $homeGame;
-
-        return $this;
-    }
-
-    /**
-     * Remove homeGame
-     *
-     * @param \TotoBundle\Entity\Game $homeGame
-     */
-    public function removeHomeGame(\TotoBundle\Entity\Game $homeGame)
-    {
-        $this->homeGames->removeElement($homeGame);
-    }
-
-    /**
-     * Get homeGames
-     *
-     * @return \Doctrine\Common\Collections\Collection
-     */
-    public function getHomeGames()
-    {
-        return $this->homeGames;
-    }
-
-    /**
-     * Add awayGame
-     *
-     * @param \TotoBundle\Entity\Game $awayGame
-     *
-     * @return Team
-     */
-    public function addAwayGame(\TotoBundle\Entity\Game $awayGame)
-    {
-        $this->awayGames[] = $awayGame;
-
-        return $this;
-    }
-
-    /**
-     * Remove awayGame
-     *
-     * @param \TotoBundle\Entity\Game $awayGame
-     */
-    public function removeAwayGame(\TotoBundle\Entity\Game $awayGame)
-    {
-        $this->awayGames->removeElement($awayGame);
-    }
-
-    /**
-     * Get awayGames
-     *
-     * @return \Doctrine\Common\Collections\Collection
-     */
-    public function getAwayGames()
-    {
-        return $this->awayGames;
-    }
-    /**
-     * @var \TotoBundle\Entity\Group
-     */
-    private $group;
-
 
     /**
      * Set group
