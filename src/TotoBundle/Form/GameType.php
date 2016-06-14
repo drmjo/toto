bf1beee37550a4cc2140b82be3cd4cd3e39de1d5
Majan Paul <drmjo007@gmail.com>
2016-06-13 21:00:07 -0700

    You can enter the game scores now

---
+++ src/TotoBundle/Form/GameType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace TotoBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GameType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('homeScore')
+            ->add('awayScore')
+            ->add('homeTeam', null, [
+                'disabled' => true,
+            ])
+            ->add('awayTeam', null, [
+                'disabled' => true,
+            ])
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TotoBundle\Entity\Game'
+        ));
+    }
+}
